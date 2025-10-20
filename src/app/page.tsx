22fb1f64d5c06b9efece7bf3254d13f3267ef79e
cnmerenu <nmerenuchima@gmail.com>
2025-10-20 21:06:43 +0200

    switch to coming soon

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,9 @@
-import Header from '@/components/Header';
-import Hero from '@/components/Hero';
-import Services from '@/components/Services';
-import About from '@/components/About';
-import CTA from '@/components/CTA';
-
 export default function Home() {
   return (
     <>
-      <Header />
-      <Hero />
-      <Services />
-      <About />
-      <CTA />
+      <div className="w-screen h-screen flex flex-col justify-center items-center bg-primary-500 text-white font-bole">
+        Coming Soon
+      </div>
     </>
   );
 }
