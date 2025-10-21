bf952e30a2d5c6dbcddb895243366fea6d9bbbed
cnmerenu <nmerenuchima@gmail.com>
2025-10-21 06:13:53 +0200

    merged

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,17 @@
+import Header from "@/components/Header";
+import Hero from "@/components/Hero";
+import Services from "@/components/Services";
+import About from "@/components/About";
+import CTA from "@/components/CTA";
+
 export default function Home() {
   return (
     <>
-      <div className="w-screen h-screen flex flex-col justify-center items-center bg-primary-500 text-white font-bole">
-        Coming Soon
-      </div>
+      <Header />
+      <Hero />
+      <Services />
+      <About />
+      <CTA />
     </>
   );
 }
